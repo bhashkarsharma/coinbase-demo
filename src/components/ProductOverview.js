eef621ef606e84c9382fb21eb33caa607badddec
Bhashkar Sharma <bhashkar.sharma@smaato.com>
2018-07-24 11:06:13 +0200

    Adding messages

--- src/components/ProductOverview.js
+++ src/components/ProductOverview.js
@@ -7,45 +7,53 @@ export default class ProductOverview extends Component {
 
     constructor() {
         super()
-        this.state = { products: [] }
+        this.state = { products: [], loadingStatus: '' }
     }
 
     componentDidMount() {
         axios.get(`${Config.API_DOMAIN}/products`)
             .then(resp => this.setState({ products: resp.data }))
-            .catch(err => console.log(err))
+            .catch(err => this.setState({ loadingStatus: 'error' }))
     }
 
     render() {
         return (
             <div>
                 <h2 className="text-center">Product Overview</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Display Name</th>
-                            <th>Base Currency</th>
-                            <th>Quote Currency</th>
-                            <th>Base min size</th>
-                            <th>Base max size</th>
-                            <th>Quote increment</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            this.state.products.map((product, key) => {
-                                return <tr key={key} className={status}>
-                                    <td><Link to={`/stats/${product.id}`}>{product.display_name}</Link></td>
-                                    <td>{product.base_currency}</td>
-                                    <td>{product.quote_currency}</td>
-                                    <td>{product.base_min_size}</td>
-                                    <td>{product.base_max_size}</td>
-                                    <td>{product.quote_increment}</td>
-                                </tr>
-                            })
+                { this.state.products.length ?
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Display Name</th>
+                                <th>Base Currency</th>
+                                <th>Quote Currency</th>
+                                <th>Base min size</th>
+                                <th>Base max size</th>
+                                <th>Quote increment</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                this.state.products.map((product, key) => {
+                                    return <tr key={key}>
+                                        <td><Link to={`/stats/${product.id}`}>{product.display_name}</Link></td>
+                                        <td>{product.base_currency}</td>
+                                        <td>{product.quote_currency}</td>
+                                        <td>{product.base_min_size}</td>
+                                        <td>{product.base_max_size}</td>
+                                        <td>{product.quote_increment}</td>
+                                    </tr>
+                                })
+                            }
+                        </tbody>
+                    </table> :
+                    <div className={'text-center m-2 p-2 ' + (this.state.loadingStatus === 'error' ? 'bg-danger' : 'bg-info') }>
+                        { this.state.loadingStatus === 'error' ?
+                            <span>There was an error loading data.</span> : 
+                            <span>Loading...</span>
                         }
-                    </tbody>
-                </table>
+                    </div>
+                }
             </div>
         )
     }
--- src/components/ProductStats.js
+++ src/components/ProductStats.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 import Config from '../config'
 
@@ -6,43 +7,52 @@ export default class ProductStats extends Component {
 
     constructor() {
         super()
-        this.state = { id: '', stats: [] }
+        this.state = { id: '', stats: {}, loadingStatus: '' }
     }
 
     componentDidMount() {
         const { id } = this.props.match.params
         axios.get(`${Config.API_DOMAIN}/products/${id}/stats`)
             .then(resp => this.setState({ id, stats: resp.data }))
-            .catch(err => console.log(err))
+            .catch(err => this.setState({ loadingStatus: 'error' }))
     }
 
     render() {
         const { open, high, low, volume, last, volume_30day } = this.state.stats
         return (
             <div>
+                <Link to="/"><div className="float-left">&laquo; Back</div></Link>
                 <h2 className="text-center">{this.state.id} Stats</h2>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <th>Open</th>
-                            <th>High</th>
-                            <th>Low</th>
-                            <th>Volume</th>
-                            <th>Last</th>
-                            <th>30 day volume</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td>{open}</td>
-                            <td>{high}</td>
-                            <td>{low}</td>
-                            <td>{volume}</td>
-                            <td>{last}</td>
-                            <td>{volume_30day}</td>
-                        </tr>
-                    </tbody>
-                </table>
+                { Object.keys(this.state.stats).length ?
+                    <table className="table">
+                        <thead>
+                            <tr>
+                                <th>Open</th>
+                                <th>High</th>
+                                <th>Low</th>
+                                <th>Volume</th>
+                                <th>Last</th>
+                                <th>30 day volume</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>{open}</td>
+                                <td>{high}</td>
+                                <td>{low}</td>
+                                <td>{volume}</td>
+                                <td>{last}</td>
+                                <td>{volume_30day}</td>
+                            </tr>
+                        </tbody>
+                    </table> :
+                    <div className={'text-center m-2 p-2 ' + (this.state.loadingStatus === 'error' ? 'bg-danger' : 'bg-info') }>
+                        { this.state.loadingStatus === 'error' ?
+                            <span>There was an error loading data.</span> : 
+                            <span>Loading...</span>
+                        }
+                    </div>
+                }
             </div>
         )
     }
